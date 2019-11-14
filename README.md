3b73d5c3858f123c5148adc794857fadcb4a39d3
cooliotonyio <tonyzhao@berkeley.edu>
2019-11-14 14:22:11 -0800

    more stuff

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 ## Setup
 
 Create virtual environment and install requirements
-```
+
+```bash
 conda create -n multiagent python=3.7
 conda activate multiagent
 pip install -r requirements.txt
-
+pip install .
 ```
 
 Create a `data` folder in master directory and download [COCO datasets](http://cocodataset.org/#home)
-*Only 2017 Val Images and Train/Val annotations are necessary for now*
-
 
 ### Project Structure
+
 ```bash
 ├── data/
 │   ├── coco/
@@ -26,12 +26,12 @@ Create a `data` folder in master directory and download [COCO datasets](http://c
 │   │   │   ├── ************.jpg
 │   │   │   └── ...
 │   │   ├── val2017/
-│   │   │   ├── ************.jpg
+│   │   │   ├── 000000000139.jpg
 │   │   │   └── ...
 ├── experiments.ipynb
 ├── multiagent/
-│   └── ... 
+│   └── ...
 ├── README.md
 ├── requirements.txt
-└── ... 
+└── ...
 ```
--- multiagent/infrastructure/trainer.py
+++ multiagent/infrastructure/trainer.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import tensorflow as tf
 
@@ -22,9 +24,32 @@ for episode in 1,m:
 class RL_Trainer(object):
 
     def __init__(self, params = None):
-        # TODO: Adapt this class
+        #############
+        ## INIT
+        #############
         self.agent = None
         self.params = params
+
+        seed = self.params["seed"]
+        tf.set_random_seed(seed)
+        np.random.seed(seed)
+
+        #############
+        ## ENVIRONMENT
+        #############
+        ob_dim = self.env.observation_space.shape if img else self.env.observation_space.shape[0]
+        ac_dim = self.env.action_space.n if discrete else self.env.action_space.shape[0]
+        self.params['agent_params']['ac_dim'] = ac_dim
+        self.params['agent_params']['ob_dim'] = ob_dim
+
+
+        #############
+        ## AGENT
+        #############
+        agent_class = self.params['agent_class']
+        self.agent = agent_class(self.sess, self.env, self.params['agent_params'])
+
+
 
     def run_training_loop(self, n_iter, collect_policy, eval_policy,
                         initial_expertdata=None, start_relabel_with_expert=1, expert_policy=None):
@@ -36,10 +61,62 @@ class RL_Trainer(object):
         :param start_relabel_with_expert: iteration at which to start relabel with expert
         :param expert_policy:
         """
+        self.start_time = time.time()
+        self.total_envsteps = 0
+
+        #TODO: implement algorithm
+
+        for iter in range(n_iter):
+            print("\n\n********** Iteration %i ************"%itr)
+
+            # Run agent
+            self.agent.step_env()
+            self.total_envsteps += 1
+
+            # Train agent (using sampled data from replay buffer)
+            losses = self.train_agent()
+
+            # log/save
+            if self.params['scalar_log_freq'] == -1:
+                self.logmetrics = False
+            elif itr % self.params['scalar_log_freq'] == 0:
+                self.logmetrics = True
+            else:
+                self.logmetrics = False
+
+            if self.logvideo or self.logmetrics:
+                # perform logging
+                print('\nBeginning logging procedure...')
+                if isinstance(self.agent, DQNAgent):
+                    self.perform_dqn_logging()
+                else:
+                    self.perform_logging(itr, paths, eval_policy, train_video_paths, all_losses)
+
+
+                # save policy
+                if self.params['save_params']:
+                    print('\nSaving agent\'s actor...')
+                    if 'actor' in dir(self.agent):
+                        self.agent.actor.save(self.params['logdir'] + '/policy_itr_'+str(itr))
+                    if 'critic' in dir(self.agent):
+                        self.agent.critic.save(self.params['logdir'] + '/critic_itr_'+str(itr))
+
+    ####################################
+    ####################################
 
     def collect_training_trajectories(self, itr):
-        pass
         # collect data to be used for training
+        print("\nCollecting data to be used for training...")
+        paths, envsteps_this_batch = sample_trajectories(self.env, collect_policy, batch_size, self.params['ep_len'])
+
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
+        # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
+        train_video_paths = None
+        if self.log_video:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            train_video_paths = sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
+
+        return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
         pass
